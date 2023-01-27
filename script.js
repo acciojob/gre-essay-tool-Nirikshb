4c10fe240d4b397de390b729ac9628f3bbc63fcf
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-01-27 19:59:06 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your code here
+// //your code here
+// const textbox = document.getElementById("evaluatedText");
+// const wordCount = document.getElementById("wordCount");
+
+// textbox.addEventListener("input", function() {
+//     let text = this.value.trim();
+//     text = text.replace(/[^a-zA-Z0-9\s]/g, ""); // remove non-alphanumeric characters
+//     wordCount.innerHTML = text.split(/\s+/).length;
+// });
+
+const texting = document.getElementById('text');
+const count = document.getElementById('word');
+
+text.addEventListener("input", function() {
+    let text = this.value.trim();
+    text = text.replace(/[^a-zA-Z0-9\s]/g, "");
+    count.innerHTML = text.split(/\s+/).length;
+})

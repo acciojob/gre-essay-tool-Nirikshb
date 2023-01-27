62697fc0c17bee9accd70b4f9a8e123efad0a74c
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-01-27 20:01:05 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+//your code here
+const textbox = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
 
-
-const texting = document.getElementById('text');
-const count = document.getElementById('word');
-
-text.addEventListener("input", function() {
+textbox.addEventListener("input", function() {
     let text = this.value.trim();
-    text = text.replace(/[^a-zA-Z0-9\s]/g, "");
-    count.innerHTML = text.split(/\s+/).length;
-})
+    text = text.replace(/[^a-zA-Z0-9\s]/g, ""); // remove non-alphanumeric characters
+    wordCount.innerHTML = text.split(/\s+/).length;
+});
